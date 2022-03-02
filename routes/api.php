a88223b3e35b4ddd56dd5a5d25d0f20510249416
Blueheart90 <chuchober@hotmail.com>
2022-03-02 11:12:46 -0500

    Create routes for each controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Transaction\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +20,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// apiResource funciona igual que Resource, pero este omite los metodos 
+// create y edit ya que en apirest no son utilizados.
+
+// Route::apiResource('buyers', BuyerController::class)->only(['index', 'show']);
+// Route::apiResource('sellers', SellerController::class)->only(['index', 'show']);
+
+Route::apiResources([
+    'buyers' => BuyerController::class,
+    'sellers' => SellerController::class,
+    'products' => ProductController::class,
+    'transactions' => TransactionController::class,
+], ['only' => ['index', 'show']]);
+
+Route::apiResources([
+    'categories' => CategoryController::class,
+    'users' => UserController::class,
+]);
