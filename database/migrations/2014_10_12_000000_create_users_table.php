23642cc2d96f6a510a5bccb07870d1033c5657a9
Blueheart90 <chuchober@hotmail.com>
2022-03-05 11:36:27 -0500

    Add migration for users
    creation fields and foreign Key constraints to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -20,6 +21,9 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('verified')->default(User::USUARIO_NO_VERIFICADO);
+            $table->string('verification_token')->nullable();
+            $table->string('admin')->default(User::USUARIO_REGULAR);
             $table->timestamps();
         });
     }
--- database/migrations/2022_03_02_151740_create_products_table.php
+++ database/migrations/2022_03_02_151740_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,16 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->unsignedInteger('quantity');
+            $table->string('status')->default(Product::PRODUCTO_NO_DISPINIBLE);
+            $table->string('image');
+            $table->unsignedInteger('seller_id');
+
+            // Se referencia users pues sellers es una herencia de user
+            $table->foreign('seller_id')->references('id')->on('users');
+
             $table->timestamps();
         });
     }
