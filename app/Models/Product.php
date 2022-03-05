9c2b7ab1e5889fac48bf6d1bb8349da5903ecdbc
Blueheart90 <chuchober@hotmail.com>
2022-03-05 10:31:06 -0500

    Add properties to models
    added some properties to the user, product, transaction and category models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    const PRODUCTO_DISPINIBLE = 'disponible';
+    const PRODUCTO_NO_DISPINIBLE = 'no disponible';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id'
+    ];
+    public function estaDisponible()
+    {
+        return $this->status == Product::PRODUCTO_DISPINIBLE;
+    }
 }
