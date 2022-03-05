75df3bf22c5e5ff589ac66d179ffbe8a018c9558
Blueheart90 <chuchober@hotmail.com>
2022-03-05 12:59:45 -0500

    Add migration for products

--- database/migrations/2022_03_02_151740_create_products_table.php
+++ database/migrations/2022_03_02_151740_create_products_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->unsignedInteger('quantity');
             $table->string('status')->default(Product::PRODUCTO_NO_DISPINIBLE);
             $table->string('image');
